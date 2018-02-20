6c6825d7d686623f5b4c2f1c85898e65dfb745b0
ElliottAYoung <elliott.a.young@gmail.com>
2018-02-20 13:49:37 -0600

    bump version and modify batching expectations

--- lib/hippo_notifier/notification_manager.rb
+++ lib/hippo_notifier/notification_manager.rb
@@ -5,20 +5,20 @@ module HippoNotifier
 
       if notification.batchable
         results << HippoNotifier::Batches::Manager.manage({ notification: notification }, client, options[:batch])
-      else
-        client.credentials.keys.each do |service_name|
-          next unless valid_service?(service_name)
+      end
 
-          args = {
-            notification: notification,
-            service_credentials: client.credentials[service_name.to_sym],
-            options: options[service_name.to_sym]
-          }
+      client.credentials.keys.each do |service_name|
+        next unless valid_service?(service_name)
 
-          service_class = service_name.to_s.split('_').map(&:capitalize).join
+        args = {
+          notification: notification,
+          service_credentials: client.credentials[service_name.to_sym],
+          options: options[service_name.to_sym]
+        }
 
-          results << HippoNotifier::Services.const_get(service_class).send('submit', args)
-        end
+        service_class = service_name.to_s.split('_').map(&:capitalize).join
+
+        results << HippoNotifier::Services.const_get(service_class).send('submit', args)
       end
 
       results
